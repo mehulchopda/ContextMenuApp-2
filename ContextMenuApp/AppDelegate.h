a79968381c074f755a892d21320753a113c57d55
mehulchopda000 <mehul.chopda@yahoo.com>
2015-08-24 11:26:03 +0200

    Initial Commit

---
+++ ContextMenuApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContextMenuApp
+//
+//  Created by Mehul Chopda on 24/08/15.
+//  Copyright (c) 2015 Mehul Chopda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
